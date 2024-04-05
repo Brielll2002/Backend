4e5fcb273b7b6da9640ead8632dd0b30ff6a8271
Gabriel Rodrigues Ventura de Araújo <133683181+Brielll2002@users.noreply.github.com>
2024-04-05 00:43:10 -0300

    Adicionei paginacao na api de home

--- src/model/home/home.js
+++ src/model/home/home.js
@@ -4,7 +4,10 @@ require('dotenv').config()
 const checkToken = require('../../token/token')
 const {home} = require('../../controller/home/HomeController')
 
-router.get('/',checkToken, async (req, res)=>{
+router.get('/:pg',checkToken, async (req, res)=>{
+
+    const pagina = req.params.pg
+
     home((err, results) => {
         if(err){
             console.error(err)
@@ -14,6 +17,7 @@ router.get('/',checkToken, async (req, res)=>{
             })
         }
         else if(results.length > 0){
+            
             const post = results.map(post => ({
                 id_post: post.id_post,
                 data: post.data,
@@ -25,10 +29,39 @@ router.get('/',checkToken, async (req, res)=>{
                 nome_curso_post: post.nome_curso_post
             }))
 
-            res.status(200).json({
-                response: true,
-                posts: post.reverse()
-            })
+            let paginas = [];
+            const reverse = post.reverse();
+            let arr = [];
+            let count = 0;
+            const base = 4;
+
+            for(let c = 0; c < reverse.length; c++){
+                if(c == (count + base)){
+                    paginas.push(arr)
+                    count = c
+                    arr=[]
+                }
+                else{
+                    arr.push(reverse[c])
+                }
+            }
+
+            if(Number(pagina) < 1 || Number(pagina) > paginas.length){
+                res.status(400).json({
+                    response: false,
+                    message: "Número de página errada. Verifique e tente novamente !",
+                    primeriapag: 1,
+                    qtdpag: paginas.length
+                })
+            }
+            else{
+                res.status(200).json({
+                    response: true,
+                    qtdpag: paginas.length,
+                    pagatual: Number(pagina),
+                    posts: paginas[Number(pagina)-1]
+                })
+            }
         }
         else{
             res.status(400).json({
