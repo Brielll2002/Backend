8d162e701b8a6f7895b9a6c8bedcb8f72cdd743a
Gabriel Rodrigues Ventura de Araújo <133683181+Brielll2002@users.noreply.github.com>
2024-04-11 01:00:05 -0300

    Update ComentarioController.js

--- src/controller/comentario/ComentarioController.js
+++ src/controller/comentario/ComentarioController.js
@@ -1,7 +1,7 @@
 const conn = require('../../conection/conn')
 
 function postarComentario(conteudo, data, nome, id_user_comentario, id_post_comentario, callback){
-    const sql = 'INSERT INTO comment (conteudo, data, nome, id_user_comentario, id_post_comentario) VALUES (?,?,?,?,?)'
+    const sql = 'INSERT INTO comment (conteudo, data, nome_usuario, id_user_comentario, id_post_comentario) VALUES (?,?,?,?,?)'
     conn.query(sql, [conteudo, data, nome, id_user_comentario, id_post_comentario], (err, results)=>{
         if(err){
             return callback(err, null)
