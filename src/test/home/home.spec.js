50b0bcbeacb1489a4910866c4f07b09f0ee94a95
Gabriel Rodrigues Ventura de Araújo <133683181+Brielll2002@users.noreply.github.com>
2024-04-14 00:44:58 -0300

    ..

--- src/test/home/home.spec.js
+++ src/test/home/home.spec.js
@@ -2,20 +2,22 @@ const app = require('../../controller/home/home');
 const request = require('supertest');
 const { gerarToken } = require('../../token/token');
 
-test('Teste rota "/"', (done) => {
+test('Teste rota "/home"', (done) => {
   const token = gerarToken(1);
 
   request(app)
     .get('/home')
     .set('Authorization', `Bearer ${token}`)
     .expect('Content-Type', /json/)
-    .expect(200 || 400)
     .end((err, res) => {
 
       console.log("Res:",res.text)
       if (err){
         return done(err);
       }
+
+      expect(res.statusCode === 200 || res.statusCode === 400 || res.statusCode === 500)
+      expect(res.body).toHaveProperty('response');
 
       done();
     });
